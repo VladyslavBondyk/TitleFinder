34b013450503545d57cc57c97f0c1b70f6365c47
VladyslavBondyk <vladyslav.bondyk@gmail.com>
2023-06-08 22:13:08 +0200

    Only for my current workflow needed. In case I get new laptop.

---
+++ TitleFinder.js
@@ -0,0 +1,5 @@
+//Use only in browser console
+var spanElements = document.querySelectorAll('span.name');
+for (var i = 0; i < spanElements.length; i++) {
+  console.log(spanElements[i].textContent);
+}
